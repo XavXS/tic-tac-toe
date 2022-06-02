910f017b935369ee7002b7f9ef26261b2b69ac05
xav <xavs421@gmail.com>
2022-06-02 14:08:44 -0400

    Add win detection comments

--- script.js
+++ script.js
@@ -26,25 +26,26 @@ const gameBoard = (() => {
     };
 
     const hasWon = (mark) => {
+        // check horizontal
         for(let i=1; i<=7; i+=3) {
             for(let j=0; j<=2; ++j) {
                 if(board[i+j] !== mark) break;
                 if(j >= 2) return true;
             }
         }
-        
+        // check vertical
         for(let i=1; i<=3; ++i) {
             for(let j=0; j<=6; j+=3) {
                 if(board[i+j] !== mark) break;
                 if(j >= 6) return true;
             }
         }
-
+        // check diagonal 1
         for(let i=1; i<=9; i+=4) {
             if(board[i] !== mark) break;
             if(i >= 9) return true;
         }
-
+        // check diagonal 2
         for(let i=3; i<=7; i+=2) {
             if(board[i] !== mark) break;
             if(i >= 7) return true;
